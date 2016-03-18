c9126dfbe23e947f95f76a8ec1bcfaad7f7e8e5f
kingjerome1 <cleanall666@yahoo.com.tw>
2016-03-18 10:20:07 +0800

    Update Easter.java

--- Easter.java
+++ Easter.java
@@ -1,74 +1,38 @@
 public class Easter {
     
-    public static String calculateEaster(int aYear){
+    int aYear,aMonth,aDay;
+		
+		Easter(){
+			
+		}
+		
+		Easter(int aYear,int aMonth,int aDay){
+			this.aYear = aYear;
+			this.aMonth = 0;
+			this.aDay = 0;
+		}
+        public void gauss_algorithm(){
         
-        
-        int a = aYear % 19,
-            b = aYear / 100,
-            c = aYear % 100,
-            d = b / 4,
-            e = b % 4,
-            g = (8 * b + 13) / 25,
-            h = (19 * a + b - d - g + 15) % 30,
-            j = c / 4,
-            k = c % 4,
-            m = (a + 11 * h) / 319,
-            r = (2 * e + 2 * j - k - h + m + 32) % 7,
-            n = (h - m + r + 90) / 25,
-            p = (h - m + r + n + 19) % 32;
 
-        String result;
-        switch(n)
-        {
-            case 1:
-                result = "January ";
-                break;
-            case 2:
-                result = "February ";
-                break;
-            case 3:
-                result = "March ";
-                break;
-            case 4:
-                result = "April ";
-                break;
-            case 5:
-                result = "May ";
-                break;
-            case 6:
-                result = "June ";
-                break;
-            case 7:
-                result = "July ";
-                break;
-            case 8:
-                result = "August ";
-                break;
-            case 9:
-                result = "September ";
-                break;
-            case 10:
-                result = "October ";
-                break;
-            case 11:
-                result = "November ";
-                break;
-            case 12:
-                result = "December ";
-                break;
-            default:
-                result = "error";
-          
-
-        }        
-        return result + p;
-        
-        
+    int a = this.aYear % 19,
+		b = this.aYear / 100,
+		c = this.aYear % 100,
+		d = b / 4,
+		e = b % 4,
+		f = (b+8) / 25,
+		g = (b-f+1) / 3,
+		h = (19*a+b-d-g+15) % 30,
+		i = c / 4,
+		k = c % 4,
+		l = (32+ 2*e + 2*i - h - k) % 7,
+		m = (a + 11*h + 22*l) / 451;
+        this.aMonth = ((h + l - 7*m + 114) / 31);
+		this.aDay = ((h + l - 7*m + 114) % 31)+1;
+              
     }     
     @Override
     public String toString() {
-        String msg1 = String.format("In 2001, Easter Sunday is: month = 4 and day = 15 ");
-        String msg2 = String.format("In 2012, Easter Sunday is: month = 4 and day = 8 ");
-        return msg1+msg2;
+        gauss_algorithm();
+        return "In " + String.format("%d", this.aYear) + ", Easter Sunday is: month = "+ String.format("%d", this.aMonth) + "and day = " + String.format("%d",this.aDay);
     }
 }
