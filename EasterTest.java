1f21bc186b49ae1ce4fc3e9c23191af0e7c385a4
kingjerome1 <cleanall666@yahoo.com.tw>
2016-03-18 09:35:13 +0800

    Update EasterTest.java

--- EasterTest.java
+++ EasterTest.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
 
+interface  Easter1{
+    void calculateEaster(int aYear);
+    void getResult(Scanner input);    
+    }
+
 class EasterTest
 {
     public static void main(String[] args)
