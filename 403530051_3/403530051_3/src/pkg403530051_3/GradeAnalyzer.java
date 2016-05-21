4cbc95dbe63ce9992212942d423343d4ab84f46f
HanselHu <apologize0616@gmail.com>
2016-05-21 20:22:44 +0800

    作業批改完成!

---
+++ 403530051_3/403530051_3/src/pkg403530051_3/GradeAnalyzer.java
@@ -0,0 +1,102 @@
+﻿package pkg403530051_3;
+import java.util.ArrayList;
+
+class MyCount { 
+    String name;
+    int cnt;
+ 
+    MyCount(){        
+    }
+    //name用來給RANK cnt用來計數
+    MyCount(String _name, int _cnt){ 
+        this.name = _name;
+        this.cnt = _cnt;
+    }
+    //使用Override
+    public String toString(){
+        return this.name + "=" + this.cnt;
+    }
+}
+
+public class GradeAnalyzer {
+   
+    int CntUserInput = 0;
+    double TotalNum = 0, SD = 0, Avg = 0, GetNum = 0, SDtemp = 0;
+    ArrayList<Double> NumScore = new ArrayList<Double>();
+    MyCount[] GradeDistri = new MyCount[11]; //陣列[0]~[10]所以需要11個空間
+    boolean FirstTime = true;
+    //使用靜態方法抓可用的數字
+    static boolean isValidGrade(double aGrade){
+        if (aGrade <= 110 && aGrade >= 0)
+            return true; //正確就true
+        return false; //錯誤就false
+    }
+    
+    void addGrade(double aGrade){ //用ArrayList的方式把每個分數分類
+	//Good, +2.
+        if (FirstTime) { 
+            GradeDistri[0] = new MyCount("A+", 0);
+            GradeDistri[1] = new MyCount("A", 0);
+            GradeDistri[2] = new MyCount("A-", 0);
+            GradeDistri[3] = new MyCount("B+", 0);
+            GradeDistri[4] = new MyCount("B", 0);
+            GradeDistri[5] = new MyCount("B-", 0);
+            GradeDistri[6] = new MyCount("C+", 0);
+            GradeDistri[7] = new MyCount("C", 0);
+            GradeDistri[8] = new MyCount("C-", 0);
+            GradeDistri[9] = new MyCount("D", 0);
+            GradeDistri[10] = new MyCount("F", 0);
+            FirstTime = false;
+        }
+        
+        if(!isValidGrade(aGrade)){
+            return;
+        }
+        NumScore.add(aGrade);
+        TotalNum += aGrade;
+        CntUserInput++;
+        if (aGrade > 90){ //將A+ A  A-進行分類
+            if (aGrade >= 98)
+                GradeDistri[0].cnt++;
+            else if (aGrade >=92)
+                GradeDistri[1].cnt++;
+            else
+                GradeDistri[2].cnt++;
+        }else if (aGrade > 80){ //將B+ B  B-進行分類
+            if (aGrade >= 88)
+                GradeDistri[3].cnt++;
+            else if (aGrade >= 82)
+                GradeDistri[4].cnt++;
+            else
+                GradeDistri[5].cnt++;            
+        }else if (aGrade > 70){  //將C+ C  C-進行分類
+            if (aGrade >=78)
+                GradeDistri[6].cnt++;
+            else if (aGrade >=72)
+                GradeDistri[7].cnt++;
+            else
+                GradeDistri[8].cnt++;
+        }else{
+            if (aGrade >= 60)  //這是D
+                GradeDistri[9].cnt++;
+            else  //這是F
+                GradeDistri[10].cnt++;
+        }
+    }
+    
+    void analyzeGrades(){
+        Avg = TotalNum / CntUserInput;  //計算平均分數和標準差
+        for (int i = 0; i < NumScore.size(); i++){
+            SDtemp += (NumScore.get(i) - Avg) * (NumScore.get(i) - Avg);
+        }  
+        SD = Math.round((Math.sqrt(SDtemp / CntUserInput)));  //應題目要求使用四捨五入法
+    }
+    //使用Override將輸出格式變成題目要求的樣子
+    public String toString(){
+        String rlt = "";
+        for (int i = 0 ; i < GradeDistri.length ; i++){
+            rlt += GradeDistri[i] + "\n";
+        }
+        return "The grade distribution is:\n\n" + rlt;
+    }
+}
---
+++ 403530051_3/403530051_3/src/pkg403530051_3/GradeAnalyzerTester.java
@@ -0,0 +1,37 @@
+﻿/*
+* [A]100
+* [TA's Advise]
+* 1. Good!
+*/
+package pkg403530051_3;
+
+import pkg403530051_3.GradeAnalyzer;
+import javax.swing.JOptionPane;
+
+public class GradeAnalyzerTester {
+    public static void main(String[] args) {
+        double TempScore = 0;  
+        String TempInput = "";  //接收使用者的輸入
+        GradeAnalyzer GA = new GradeAnalyzer();  //使用GradeAnalyzer
+        
+        while(true){ //當輸入東西的時候進入下一行
+            TempInput = JOptionPane.showInputDialog(null, "!!!請輸入一個分數!!!");
+            if (TempInput.equalsIgnoreCase("q")){ //當輸入q或Q的時候離開   
+                break;
+            }
+            try{ //用try catch把不合規定的數字抓出來
+                GA.addGrade(Double.parseDouble(TempInput));
+            } catch (Exception e) {
+                System.out.println("請輸入可用的數字!");
+            }
+        }
+        //缺少判斷是否<2, -2.
+        GA.analyzeGrades();
+        //列出輸出格式
+        System.out.println("You entered " + GA.CntUserInput + "Valid grades from 0 to 110.  Invalid grades are ignored! ");
+        //顯示出平均和標準差
+        System.out.println("The average = " + (int) GA.Avg + " with a standard deviation = " + (int) GA.SD);
+        //將GradeAnalyzer中toString()內的文字顯示出來
+        System.out.println(GA);
+    }
+}        
